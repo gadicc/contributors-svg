9199893eea6e33f795aac38df28c353a1013787b
Gadi Cohen <dragon@wastelands.net>
2021-09-03 10:14:16 +0300

    chore(prettier): prettify files

--- api/svg.js
+++ api/svg.js
@@ -1,4 +1,4 @@
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 
 const TEST_MODE = !!process.env.TEST_MODE;
 
@@ -12,14 +12,18 @@ const MARGIN_Y = MARGIN;
 
 const MAX_WIDTH = 890;
 
-const SKIP = [ "semantic-release-bot", "renovate-bot" ];
+const SKIP = ["semantic-release-bot", "renovate-bot"];
 
 async function dataUrl(url) {
   const res = await fetch(url);
   if (res.ok) {
     const buffer = await res.buffer();
-    return "data:" + res.headers.get("content-type") + ";base64,"
-      + buffer.toString('base64');
+    return (
+      "data:" +
+      res.headers.get("content-type") +
+      ";base64," +
+      buffer.toString("base64")
+    );
   }
 }
 
@@ -27,64 +31,64 @@ module.exports = async (req, res) => {
   const user = req.query.user;
   const repo = req.query.repo;
   const dataUri = req.query.dataUri === "false" ? false : true;
-  
+
   const url = `https://github.com/${user}/${repo}/graphs/contributors-data`;
 
   let data;
   if (TEST_MODE) {
-    data = require('../test.js');
+    data = require("../test.js");
   } else {
     const dataRes = await fetch(url, {
-      "headers": {
-        "accept": "application/json",
+      headers: {
+        accept: "application/json",
       },
     });
     data = await dataRes.json();
   }
-  
-  let innerSvg = '';
+
+  let innerSvg = "";
   let row = 0;
   let totalRows = 1;
   let x = 0;
   let y = 0;
-  data.sort((a,b) => b.total - a.total);
+  data.sort((a, b) => b.total - a.total);
   for (let contrib of data) {
     const author = contrib.author;
-    if (SKIP.includes(author.login))
-      continue;
-    
+    if (SKIP.includes(author.login)) continue;
+
     // https://avatars.githubusercontent.com/u/381978?s=60&amp;v=4
-    const avatarUrl = (TEST_MODE || !dataUri)
-      ? author.avatar.replace(/&/g,'&amp;')
-      : await dataUrl(author.avatar);
-    
-    innerSvg += `  <a xlink:href="https://github.com/${author.login}" target="_blank" rel="nofollow" id="${author.login}">\n`
-      + `    <clipPath id="cp-${author.login}">\n`
-      + `      <circle r="${RADIUS}" cx="${x+RADIUS}" cy="${y+RADIUS}" />\n`
-      + `    </clipPath>\n`
-      + `    <image x="${x}" y="${y}" clip-path="url(#cp-${author.login})" width="${IMG_W}" height="${IMG_H}" xlink:href="${avatarUrl}" />  \n`
-      + `    <title>${author.login} (${contrib.total})</title>\n`
-      + `  </a>\n`;
+    const avatarUrl =
+      TEST_MODE || !dataUri
+        ? author.avatar.replace(/&/g, "&amp;")
+        : await dataUrl(author.avatar);
+
+    innerSvg +=
+      `  <a xlink:href="https://github.com/${author.login}" target="_blank" rel="nofollow" id="${author.login}">\n` +
+      `    <clipPath id="cp-${author.login}">\n` +
+      `      <circle r="${RADIUS}" cx="${x + RADIUS}" cy="${y + RADIUS}" />\n` +
+      `    </clipPath>\n` +
+      `    <image x="${x}" y="${y}" clip-path="url(#cp-${author.login})" width="${IMG_W}" height="${IMG_H}" xlink:href="${avatarUrl}" />  \n` +
+      `    <title>${author.login} (${contrib.total})</title>\n` +
+      `  </a>\n`;
     x += IMG_W + MARGIN_X;
   }
-  
-  
+
   const width = x - MARGIN_X;
   const height = totalRows * (IMG_H + MARGIN_Y) - MARGIN_Y;
 
-  let svg = '<svg\n'
-    + '  xmlns="http://www.w3.org/2000/svg"\n'
-    + '  xmlns:xlink="http://www.w3.org/1999/xlink"\n'
-    + `  width="${width}"\n`
-    + `  height="${height}"\n`
-    + '>\n'
-    + "  <style>\n"
-    + "  </style>\n";
-  
-  svg += innerSvg + '</svg>\n';
-  
-  res.setHeader('cache-control', 'public, max-age=60');
-  res.setHeader('content-type', 'image/svg+xml; charset=utf-8');
+  let svg =
+    "<svg\n" +
+    '  xmlns="http://www.w3.org/2000/svg"\n' +
+    '  xmlns:xlink="http://www.w3.org/1999/xlink"\n' +
+    `  width="${width}"\n` +
+    `  height="${height}"\n` +
+    ">\n" +
+    "  <style>\n" +
+    "  </style>\n";
+
+  svg += innerSvg + "</svg>\n";
+
+  res.setHeader("cache-control", "public, max-age=60");
+  res.setHeader("content-type", "image/svg+xml; charset=utf-8");
   res.end(svg);
-  
 };
--- test.js
+++ test.js
@@ -1,182 +1,182 @@
 module.exports = [
   {
-    "total": 13,
-    "author": {
-      "id": 25180681,
-      "login": "renovate-bot",
-      "avatar": "https://avatars.githubusercontent.com/u/25180681?s=60&v=4",
-      "path": "/renovate-bot",
-      "hovercard_url": "/users/renovate-bot/hovercard"
+    total: 13,
+    author: {
+      id: 25180681,
+      login: "renovate-bot",
+      avatar: "https://avatars.githubusercontent.com/u/25180681?s=60&v=4",
+      path: "/renovate-bot",
+      hovercard_url: "/users/renovate-bot/hovercard",
     },
-    "weeks": [
+    weeks: [
       {
-        "w": 1610841600,
-        "a": 0,
-        "d": 0,
-        "c": 0
+        w: 1610841600,
+        a: 0,
+        d: 0,
+        c: 0,
       },
       {
-        "w": 1611446400,
-        "a": 41,
-        "d": 41,
-        "c": 4
+        w: 1611446400,
+        a: 41,
+        d: 41,
+        c: 4,
       },
       {
-        "w": 1612051200,
-        "a": 14,
-        "d": 14,
-        "c": 4
+        w: 1612051200,
+        a: 14,
+        d: 14,
+        c: 4,
       },
       {
-        "w": 1612656000,
-        "a": 35,
-        "d": 30,
-        "c": 5
-      }
-    ]
+        w: 1612656000,
+        a: 35,
+        d: 30,
+        c: 5,
+      },
+    ],
   },
   {
-    "total": 2,
-    "author": {
-      "id": 67528879,
-      "login": "PythonCreator27",
-      "avatar": "https://avatars.githubusercontent.com/u/67528879?s=60&v=4",
-      "path": "/PythonCreator27",
-      "hovercard_url": "/users/PythonCreator27/hovercard"
+    total: 2,
+    author: {
+      id: 67528879,
+      login: "PythonCreator27",
+      avatar: "https://avatars.githubusercontent.com/u/67528879?s=60&v=4",
+      path: "/PythonCreator27",
+      hovercard_url: "/users/PythonCreator27/hovercard",
     },
-    "weeks": [
+    weeks: [
       {
-        "w": 1610841600,
-        "a": 0,
-        "d": 0,
-        "c": 0
+        w: 1610841600,
+        a: 0,
+        d: 0,
+        c: 0,
       },
       {
-        "w": 1611446400,
-        "a": 0,
-        "d": 0,
-        "c": 0
+        w: 1611446400,
+        a: 0,
+        d: 0,
+        c: 0,
       },
       {
-        "w": 1612051200,
-        "a": 9,
-        "d": 5,
-        "c": 2
+        w: 1612051200,
+        a: 9,
+        d: 5,
+        c: 2,
       },
       {
-        "w": 1612656000,
-        "a": 0,
-        "d": 0,
-        "c": 0
-      }
-    ]
+        w: 1612656000,
+        a: 0,
+        d: 0,
+        c: 0,
+      },
+    ],
   },
   {
-    "total": 14,
-    "author": {
-      "id": 32174276,
-      "login": "semantic-release-bot",
-      "avatar": "https://avatars.githubusercontent.com/u/32174276?s=60&v=4",
-      "path": "/semantic-release-bot",
-      "hovercard_url": "/users/semantic-release-bot/hovercard"
+    total: 14,
+    author: {
+      id: 32174276,
+      login: "semantic-release-bot",
+      avatar: "https://avatars.githubusercontent.com/u/32174276?s=60&v=4",
+      path: "/semantic-release-bot",
+      hovercard_url: "/users/semantic-release-bot/hovercard",
     },
-    "weeks": [
+    weeks: [
       {
-        "w": 1610841600,
-        "a": 0,
-        "d": 0,
-        "c": 0
+        w: 1610841600,
+        a: 0,
+        d: 0,
+        c: 0,
       },
       {
-        "w": 1611446400,
-        "a": 51,
-        "d": 0,
-        "c": 5
+        w: 1611446400,
+        a: 51,
+        d: 0,
+        c: 5,
       },
       {
-        "w": 1612051200,
-        "a": 81,
-        "d": 0,
-        "c": 8
+        w: 1612051200,
+        a: 81,
+        d: 0,
+        c: 8,
       },
       {
-        "w": 1612656000,
-        "a": 7,
-        "d": 0,
-        "c": 1
-      }
-    ]
+        w: 1612656000,
+        a: 7,
+        d: 0,
+        c: 1,
+      },
+    ],
   },
   {
-    "total": 171,
-    "author": {
-      "id": 381978,
-      "login": "gadicc",
-      "avatar": "https://avatars.githubusercontent.com/u/381978?s=60&v=4",
-      "path": "/gadicc",
-      "hovercard_url": "/users/gadicc/hovercard"
+    total: 171,
+    author: {
+      id: 381978,
+      login: "gadicc",
+      avatar: "https://avatars.githubusercontent.com/u/381978?s=60&v=4",
+      path: "/gadicc",
+      hovercard_url: "/users/gadicc/hovercard",
     },
-    "weeks": [
+    weeks: [
       {
-        "w": 1610841600,
-        "a": 4836,
-        "d": 291,
-        "c": 11
+        w: 1610841600,
+        a: 4836,
+        d: 291,
+        c: 11,
       },
       {
-        "w": 1611446400,
-        "a": 6176,
-        "d": 712,
-        "c": 38
+        w: 1611446400,
+        a: 6176,
+        d: 712,
+        c: 38,
       },
       {
-        "w": 1612051200,
-        "a": 22160,
-        "d": 4597,
-        "c": 91
+        w: 1612051200,
+        a: 22160,
+        d: 4597,
+        c: 91,
       },
       {
-        "w": 1612656000,
-        "a": 5495,
-        "d": 2179,
-        "c": 31
-      }
-    ]
+        w: 1612656000,
+        a: 5495,
+        d: 2179,
+        c: 31,
+      },
+    ],
   },
   {
-    "total": 6,
-    "author": {
-      "id": 468498,
-      "login": "pudgereyem",
-      "avatar": "https://avatars.githubusercontent.com/u/468498?s=60&v=4",
-      "path": "/pudgereyem",
-      "hovercard_url": "/users/pudgereyem/hovercard"
+    total: 6,
+    author: {
+      id: 468498,
+      login: "pudgereyem",
+      avatar: "https://avatars.githubusercontent.com/u/468498?s=60&v=4",
+      path: "/pudgereyem",
+      hovercard_url: "/users/pudgereyem/hovercard",
     },
-    "weeks": [
+    weeks: [
       {
-        "w": 1610841600,
-        "a": 0,
-        "d": 0,
-        "c": 0
+        w: 1610841600,
+        a: 0,
+        d: 0,
+        c: 0,
       },
       {
-        "w": 1611446400,
-        "a": 0,
-        "d": 0,
-        "c": 0
+        w: 1611446400,
+        a: 0,
+        d: 0,
+        c: 0,
       },
       {
-        "w": 1612051200,
-        "a": 305,
-        "d": 125,
-        "c": 1
+        w: 1612051200,
+        a: 305,
+        d: 125,
+        c: 1,
       },
       {
-        "w": 1612656000,
-        "a": 617,
-        "d": 15,
-        "c": 5
-      }
-    ]
-  }
+        w: 1612656000,
+        a: 617,
+        d: 15,
+        c: 5,
+      },
+    ],
+  },
 ];
